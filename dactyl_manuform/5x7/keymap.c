9ecd58c1fd525aafc8cb98e16cc53b69e1ea03ca
Chris <fleetscut@gmail.com>
2022-06-07 18:38:25 -0400

    added keymaps

--- dactyl_manuform/5x7/keymap.c
+++ dactyl_manuform/5x7/keymap.c
@@ -4,9 +4,11 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _FN     1
-#define _NUMPAD 2
+#define _DHM 0
+#define _FN  1
+#define _NUM 2
+#define _SYM 3
+#define _NAV 4
 
 // Some basic macros
 #define TASK   LCTL(LSFT(KC_ESC))
@@ -14,50 +16,101 @@
 #define TAB_L  LCTL(LSFT(KC_TAB))
 #define TAB_RO LCTL(LSFT(KC_T))
 
+// Home Row Mods
+#define SHFT_T LSFT_T(KC_T)
+#define CTL_S LCTL_T(KC_S)
+#define ALT_R LALT_T(KC_R)
+#define GUI_A LGUI_T(KC_A)
+
+#define SFT_N RSFT_T(KC_N)
+#define CTL_E RCTL_T(KC_E)
+#define ALT_I RALT_T(KC_I)
+#define GUI_O RGUI_T(KC_O)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_5x7(
+    [_DHM] = LAYOUT_5x7(
         // left hand
-        KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TAB_RO,
-        OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-        KC_CAPS,   KC_LGUI, TAB_L,   TAB_R,
-                                    TT(_FN), KC_SPC,
-                                    KC_END, KC_HOME,
-                                    KC_PSCR, TASK,
+        _______,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_ESC,
+        _______,    KC_Q,    KC_W,    KC_F,   KC_P,   KC_B,   _______,
+        KC_EQL,   GUI_A,    ALT_R,    CTL_S,   SHFT_T,   KC_G,   KC_MINUS,
+        _______,   KC_Z,    KC_X,    KC_C,   KC_D,   KC_V,
+        _______,   _______, KC_LEFT,   KC_RIGHT,
+                                    KC_PGDN, KC_PGUP,
+                                    KC_END, LT(_NAV,KC_SPC),
+                                    KC_DEL, KC_BSPC,
         // right hand
-                          KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
-                          KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-                      TG(_NUMPAD), KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),
-                                            KC_LEFT, KC_UP,    KC_DOWN,  KC_RGHT,
-             KC_BSPC, KC_ENT,
-             KC_PGUP, KC_PGDN,
-             KC_LCTL, KC_LALT
+                          KC_ESC, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     _______,
+                          _______, KC_J,    KC_L,    KC_U,     KC_Y,     KC_SCLN,     KC_BSLS,
+                          KC_UNDS, KC_M,    SFT_N,    CTL_E,     ALT_I,     GUI_O,  KC_QUOT,
+                                   KC_K,    KC_H,    KC_COMM,  KC_DOT,   KC_SLSH,  _______,
+                                            KC_DOWN,   KC_UP,  _______,  _______,
+             LT(_FN,KC_HOME), KC_END,
+             LT(_SYM,KC_ENTER), KC_PGDN,
+             LT(_NUM,KC_TAB), RSFT_T(KC_CAPS)
     ),
 
     [_FN] = LAYOUT_5x7(
         // left hand
-        _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,    KC_F6,
-        _______,   _______,   _______,    KC_UP,    _______,   _______,  _______,
-        _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  QK_BOOT,
-        _______,   _______,   _______,   _______,   _______,   _______,
-        KC_MSTP,   KC_MPLY,   KC_MPRV,   KC_MNXT,
+        QK_REBOOT,   _______,     _______,      _______,    _______,     _______,    QK_BOOT,
+        _______,   KC_F12,   KC_F7,    KC_F8,    KC_F9,   KC_PSCR,  _______,
+        _______,   KC_F11,   KC_F4,    KC_F5,  KC_F6,   KC_SCRL,  _______,
+        _______,   KC_F10,   KC_F1,   KC_F2,   KC_F3,   KC_PAUSE,
+        _______,   _______,   _______,   _______,
                                     _______, _______,
                                     _______, _______,
                                     _______, _______,
         // right hand
-                          KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
+                          _______,     _______,     _______,     _______,    _______,    _______,    _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                                      _______,   _______,   _______,   _______,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
-             KC_DEL, _______,
+             _______, _______,
              _______, _______,
              _______, _______
     ),
 
-    [_NUMPAD] = LAYOUT_5x7(
+    [_NUM] = LAYOUT_5x7(
+        // left hand
+        _______,   _______,   _______,   _______,   _______,   _______,  _______,
+        _______,   _______,   KC_7,   KC_8,   KC_9,   _______,  _______,
+        _______,   _______,   KC_4,   KC_5,   KC_6,   _______,  _______,
+        _______,   _______,   KC_1,   KC_2,   KC_3,   _______,
+        _______,   _______,   KC_0,   KC_DOT,
+                                    _______, _______,
+                                    _______, _______,
+                                    _______, _______,
+        // right hand
+                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                          _______,   _______,   _______,     _______,     _______,     _______,   _______,
+                          _______,   _______,   _______,     _______,     _______,     _______,   _______,
+                                     _______,   _______,     _______,     _______,     _______,   _______,
+                                                           _______,     _______,   _______,   _______,
+             _______, _______,
+             _______, _______,
+             _______, _______
+    ),
+    [_SYM] = LAYOUT_5x7(
+        // left hand
+        _______,   _______,   _______,   _______,   _______,   _______,  QK_BOOT,
+        _______,   KC_LCBR,   KC_AMPR,   KC_ASTR,   KC_TILDE,   KC_RCBR,  _______,
+        _______,   KC_LPRN,   KC_DLR,   KC_PERC,   KC_CIRC,   KC_RPRN,  KC_GRV,
+        _______,   KC_LBRC,   KC_EXLM,   KC_AT,   KC_HASH,   KC_RBRC,
+        _______,   _______,   _______,   _______,
+                                    _______, _______,
+                                    _______, _______,
+                                    _______, _______,
+        // right hand
+                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                          _______,   _______,   _______,     _______,     _______,     _______,   _______,
+                          _______,   _______,   _______,     _______,     _______,     _______,   _______,
+                                     _______,   _______,     _______,     _______,     _______,   _______,
+                                                           _______,     _______,   _______,   _______,
+             _______, _______,
+             _______, _______,
+             _______, _______
+    ),
+    [_NAV] = LAYOUT_5x7(
         // left hand
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
@@ -68,12 +121,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______,
                                     _______, _______,
         // right hand
-                          _______,   _______,   KC_NLCK,   _______,   KC_PMNS,   KC_PPLS,   _______,
-                          _______,   _______,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
-                          _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
-                                     _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
-                                                           KC_P0,     KC_PDOT,   _______,   _______,
-             _______, KC_PENT,
+                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                          _______,   _______,   _______,     _______,     _______,     _______,   _______,
+                          _______,   KC_CAPS,   KC_LEFT,     KC_DOWN,     KC_UP,     KC_RIGHT,   _______,
+                                     KC_INS,   KC_HOME,     KC_PGDN,     KC_PGUP,     KC_END,   _______,
+                                                           _______,     _______,   _______,   _______,
+             _______, _______,
              _______, _______,
              _______, _______
     ),
